67a2e9394314354247556232e997428eda6c5d52
Amir Azadeh Ranjbar <amirazadehranjbar@gmail.com>
2025-05-17 11:44:58 -0800

    commit 5

--- src/components/contact/addContact.jsx
+++ src/components/contact/addContact.jsx
@@ -1,6 +1,57 @@
+import {useState} from "react";
+import axios from "axios";
+import swal from 'sweetalert';
+
 const AddContact = () => {
+
+    const [newUserData, setNewUserData] = useState({});
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setNewUserData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSaveContact = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            await axios.post("http://localhost:8000/contact", newUserData).then((res) => {
+                if (res.status === 201) {
+                    swal({
+                        title: "Contact Added",
+                        text: "Contact added successfully",
+                        icon: "success",
+                        button: "OK",
+                    }).then(() => {
+                        window.history.back();
+                    });
+                } else {
+                    swal({
+                        title: "Error",
+                        text: res.data.message,
+                        icon: "error",
+                        button: "OK",
+                    });
+                }
+            });
+
+        } catch (err) {
+            console.log(err);
+            await swal({
+                title: "Error",
+                text: err.message,
+                icon: "error",
+                button: "OK",
+            });
+        }
+    }
+
     return (
-        <div className="w-full h-[calc(100vh-64px)] flex justify-center items-center bg-slate-700 ">
+        <div className="w-full h-[calc(100vh-64px)] flex justify-center items-center bg-slate-700 overflow-y-auto">
             <div
                 className="flex flex-col w-80 bg-slate-400 rounded-md border border-gray-200 shadow-md p-4 space-y-8">
                 {/*region contact name input */}
@@ -12,6 +63,7 @@ const AddContact = () => {
                         name="name"
                         className="w-full border border-gray-300 rounded-md p-2 mt-2"
                         placeholder="Enter name"
+                        onChange={handleInputChange}
                     />
                 </div>
                 {/*endregion*/}
@@ -25,6 +77,7 @@ const AddContact = () => {
                         name="email"
                         className="w-full border border-gray-300 rounded-md p-2 mt-2"
                         placeholder="Enter email"
+                        onChange={handleInputChange}
                     />
                 </div>
                 {/*endregion*/}
@@ -38,6 +91,7 @@ const AddContact = () => {
                         name="phone"
                         className="w-full border border-gray-300 rounded-md p-2 mt-2"
                         placeholder="Enter phone"
+                        onChange={handleInputChange}
                     />
                 </div>
                 {/*endregion*/}
@@ -51,6 +105,7 @@ const AddContact = () => {
                         name="image"
                         className="w-full border border-gray-300 rounded-md p-2 mt-2"
                         placeholder="Enter image url"
+                        onChange={handleInputChange}
                     />
                 </div>
                 {/*endregion*/}
@@ -60,6 +115,7 @@ const AddContact = () => {
                     <button
                         className="w-full bg-slate-800 text-slate-300 font-bold py-2 px-4 rounded-md
                         hover:bg-blue-900 transition duration-200 cursor-pointer"
+                        onClick={handleSaveContact}
                     >
                         Add Contact
                     </button>
@@ -67,12 +123,14 @@ const AddContact = () => {
                     <button
                         className="w-full bg-slate-800 text-slate-300 font-bold py-2 px-4 rounded-md
                         hover:bg-blue-900 transition duration-200 cursor-pointer"
+                        onClick={() => {
+                            window.history.back();
+                        }}
                     >
                         Back
                     </button>
                 </div>
-
-
+                {/*endregion*/}
             </div>
         </div>
     );
